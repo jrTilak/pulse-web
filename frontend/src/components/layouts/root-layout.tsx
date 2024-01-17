8c328b3ed117805ad69dfe199dee2f9c74975bd4
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-17 18:55:37 +0545

    Update root layout component

--- frontend/src/components/layouts/root-layout.tsx
+++ frontend/src/components/layouts/root-layout.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
-// import SidebarComp from "../shared/sidebar/sidebar-comp";
-// import SidebarHeader from "../shared/sidebar/sidebar-header";
+import SidebarComp from "../shared/sidebar/sidebar-comp";
+import SidebarHeader from "../shared/sidebar/sidebar-header";
 import { ScrollArea } from "../ui/scroll-area";
 import { height } from "@/assets/constants/styles";
 // import CallingPage from "../pages/call/calling-page";
@@ -8,8 +8,8 @@ import { height } from "@/assets/constants/styles";
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex md:flex-row flex-col-reverse">
-      {/* <SidebarHeader />
-      <SidebarComp /> */}
+      <SidebarHeader />
+      <SidebarComp />
       <ScrollArea className={cn("md:h-screen w-full", height.max)}>
         <div className={cn("mx-auto", height.max)}>{children}</div>
       </ScrollArea>
