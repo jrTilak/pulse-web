d1d44b3ca52aa69b82e2f6694a561900d92183c9
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-17 10:44:31 +0545

    😶‍🌫️feat: add route to get and post files

--- backend/src/routes/chat-route.ts
+++ backend/src/routes/chat-route.ts
@@ -4,7 +4,13 @@ import ChatController from "../controllers/chat-controllers";
 
 const chatRouter = Router();
 
+/**
+ * @GET - routes
+ */
+
+// Get last chats
 chatRouter.get("/last-chats", verifyToken, ChatController.getLastChats);
+// Get all chats
 chatRouter.get("/:chatId", verifyToken, ChatController.getAllChats);
 
 export default chatRouter;
