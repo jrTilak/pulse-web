a10f6a76b9508ef983c557cccad1dd1b8ea19a11
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-17 10:31:35 +0545

    Update index.ts to include auth route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import authRouter from "./auth-route";
 
 const appRouter = Router();
 
 appRouter.get("/", (req, res) => {
   res.status(200).json({
-    message: "Welcome to the API",
+    message: "Welcome to the API of Pulse Backend!",
   });
 });
+
+appRouter.use("/auth", authRouter);
 
 export default appRouter;
