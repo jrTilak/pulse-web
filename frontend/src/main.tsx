82561265ec5d7467e86a2913abdb5376b4f1eb80
Tilak Thapa <iamjrtilak@gmail.com>
2024-01-17 20:52:49 +0545

    Enable GlobalContextProvider in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,16 @@ import "./assets/styles/index.css";
 import { Toaster } from "./components/ui/toaster.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context.tsx";
-// import { GlobalContextProvider } from "./contexts/global-context.tsx";
+import { GlobalContextProvider } from "./contexts/global-context.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   <BrowserRouter>
     <AuthProvider>
-      {/* <GlobalContextProvider> */}
+      <GlobalContextProvider>
         <App />
         <Toaster />
-      {/* </GlobalContextProvider> */}
+      </GlobalContextProvider>
     </AuthProvider>
   </BrowserRouter>
   // </React.StrictMode>
